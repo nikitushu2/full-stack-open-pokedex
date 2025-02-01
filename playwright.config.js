909a410d2c42e0b9d863049b87141280025776c2
Nikita Shub <nikitushu2@gmail.com>
2025-02-01 18:20:24 +0200

    fix tests ci

--- playwright.config.js
+++ playwright.config.js
@@ -6,10 +6,11 @@ export default defineConfig({
         command: 'npm run start-prod', // Change this based on your app's start command
         port: 5000, // Change this to your app's port
         timeout: 120 * 1000, // Increase if your app takes longer to start
+        // eslint-disable-next-line no-undef
         reuseExistingServer: !process.env.CI, // Reuse server for faster local runs
     },
     use: {
         baseURL: 'http://localhost:5000', // Set to match your app's running URL
         headless: true, // Set false for debugging
     },
-});
+})
